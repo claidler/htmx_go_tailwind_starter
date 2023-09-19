b616e33a0ec2b2561352757f3716c962d54f6e21
Chris Laidler <cjjlaidler@gmail.com>
2023-09-19 21:43:43 +0100

    feat: add browsersync middleware for caching

--- dev.sh
+++ dev.sh
@@ -7,3 +7,7 @@ browser-sync start \
   --files 'public/**/*.html, public/**/*.css' \
   --port 3001 \
   --proxy 'localhost:3000' \
+  --middleware 'function(req, res, next) { \
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); \
+    return next(); \
+  }'
--- index.go
+++ index.go
@@ -5,32 +5,23 @@ import (
 	"net/http"
 )
 
-func NoCache(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
-		w.Header().Set("Pragma", "no-cache")
-		w.Header().Set("Expires", "0")
-		h.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	fs := http.FileServer(http.Dir("public"))
-	http.Handle("/styles/", NoCache(http.StripPrefix("/styles/", http.FileServer(http.Dir("public")))))
+	http.Handle("/api/styles/", http.StripPrefix("/api/styles/", http.FileServer(http.Dir("public"))))
 
-	http.Handle("/sidebar", NoCache(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	http.Handle("/api/sidebar", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "public/components/layout/sidebar.html")
-	})))
-	
-	http.Handle("/header", NoCache(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	}))
+
+	http.Handle("/api/header", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "public/components/layout/header.html")
-	})))
-	
-	http.Handle("/dashboard", NoCache(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	}))
+
+	http.Handle("/api/dashboard", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "public/components/layout/dashboard.html")
-	})))
-	
-	http.Handle("/", NoCache(fs))
+	}))
+
+	http.Handle("/", fs)
 
 	log.Println("Listening on :3000...")
 	err := http.ListenAndServe(":3000", nil)
