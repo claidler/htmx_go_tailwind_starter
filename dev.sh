83e0a8a7aa5659ee89a1749665c5c822f67a140a
Chris Laidler <cjjlaidler@gmail.com>
2023-09-19 21:42:10 +0100

    feat: remove redundant packages

--- dev.sh
+++ dev.sh
@@ -1,1 +1,9 @@
-concurrently "air -c ./.air.toml" "npx tailwind -i 'styles.css' -o 'public/styles.css' --watch" "browser-sync start --files 'public/**/*.html, public/**/*.css' --port 3001 --proxy 'localhost:3000'"
+air -c ./.air.toml & \
+npx tailwind \
+  -i 'styles.css' \
+  -o 'public/styles.css' \
+  --watch & \
+browser-sync start \
+  --files 'public/**/*.html, public/**/*.css' \
+  --port 3001 \
+  --proxy 'localhost:3000' \
